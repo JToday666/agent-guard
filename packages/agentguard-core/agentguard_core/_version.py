5e9d928390a7463ed3873bb50842564a8e7aa296
TODAY <1154362900@qq.com>
2026-08-05 13:21:38 +0800

    refactor(packaging): unify Python distributions under AEGIS

--- packages/agentguard-core/agentguard_core/_version.py
+++ packages/agentguard-core/agentguard_core/_version.py
@@ -3,6 +3,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("agentguard-core")
+    __version__ = version("aegis-agentguard-core")
 except PackageNotFoundError:  # pragma: no cover - source tree without metadata
     __version__ = "0.1.0b1"
